21914c19d78c336d3c338a81803e88cf464d9193
alan <alanvela00@gmail.com>
2022-11-24 17:40:39 -0600

    update code

---
+++ src/screens/Display3DAvatar/avatar_styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+// these are the styles for 3D avatar screen GUI
+const styles = StyleSheet.create({
+  parentContainer: {
+    display: "flex",
+    flex: 1,
+  },
+});
+
+export default styles;
---
+++ src/screens/Display3DAvatar/index.js
@@ -0,0 +1,2 @@
+import ThreeDAvatar from './3dAvatar';
+export default ThreeDAvatar;
